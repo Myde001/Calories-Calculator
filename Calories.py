b93f5aac5ad1dabea97a3872286b7badd604e364
Myde001 <54612217+Myde001@users.noreply.github.com>
2020-10-01 23:11:09 -0400

    Update Calories.py

--- Calories.py
+++ Calories.py
@@ -7,9 +7,13 @@ Created on Wed Sep 23 00:45:03 2020
 """
 
 from clarifai.rest import ClarifaiApp, Image as ClImage
-from dotenv import load_dotenv
+from pathlib import Path 
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 import wolframalpha
+
 
 
 class Calories:
